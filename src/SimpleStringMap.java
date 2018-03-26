bca07637b14391bb75a6e63f4c8bfb84c75e9302
Wojciech Kubaty <wkubaty@gmail.com>
2018-03-27 01:07:37 +0200

    Initial commit

---
+++ src/SimpleStringMap.java
@@ -0,0 +1,10 @@
+public interface SimpleStringMap {
+
+    boolean containsKey(String key);
+
+    String get(String key);
+
+    String put(String key, String value);
+
+    String remove(String key);
+}
---
+++ src/StringMap.java
@@ -0,0 +1,6 @@
+import java.util.HashMap;
+
+public class StringMap implements SimpleStringMap{
+    private HashMap<String, String> hashMap;
+
+}
